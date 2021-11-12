4efb9bf83b5c7b7a6ee181ca2461447d7351f8cf
Wilson Neely <Naruisk.art@gmail.com>
2021-11-11 18:11:28 -0600

    add router to constructor, add empty constructor

--- src/main/java/com/revature/project0/render/screens/View.java
+++ src/main/java/com/revature/project0/render/screens/View.java
@@ -1,16 +1,24 @@
 package com.revature.project0.render.screens;
 
+import com.revature.project0.util.ScreenRouter;
+
 import java.util.Scanner;
 
 public abstract class View {
     protected String name;
     protected String route;
     protected Scanner consoleScanner;
+    protected ScreenRouter router;
 
-    public View(String name, String route, Scanner consoleScanner) {
+    public View(){
+
+    }
+
+    public View(String name, String route, Scanner consoleScanner, ScreenRouter router) {
         this.name = name;
         this.route = route;
         this.consoleScanner = consoleScanner;
+        this.router = router;
     }
 
     public String getRoute() {

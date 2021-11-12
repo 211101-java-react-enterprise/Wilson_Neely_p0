8613f8d06427a017f826d14c0a56cf6ae5ee7bad
Wilson Neely <Naruisk.art@gmail.com>
2021-11-11 18:10:20 -0600

    Consolidate TransactionHistory back into one

--- src/main/java/com/revature/project0/render/screens/inner/TransactionHist.java
+++ src/main/java/com/revature/project0/render/screens/inner/TransactionHist.java
@@ -1,12 +1,14 @@
 package com.revature.project0.render.screens.inner;
 
 import com.revature.project0.render.screens.View;
+import com.revature.project0.util.ScreenRouter;
 
 import java.util.Scanner;
 
-public abstract class TransactionHist extends View {
-    public TransactionHist(String name, String route, Scanner consoleScanner) {
-        super(name, route, consoleScanner);
+public class TransactionHist extends View {
+
+    public TransactionHist(Scanner consoleScanner, ScreenRouter router) {
+        super("TransactionHistory", "loggedin/transactionhistory", consoleScanner, router);
     }
 
     @Override
--- src/main/java/com/revature/project0/render/screens/inner/TransactionHistSent.java
+++
@@ -1,9 +0,0 @@
-package com.revature.project0.render.screens.inner;
-
-import java.util.Scanner;
-
-public class TransactionHistSent extends TransactionHist {
-    public TransactionHistSent(String name, String route, Scanner consoleScanner) {
-        super(name, route, consoleScanner);
-    }
-}

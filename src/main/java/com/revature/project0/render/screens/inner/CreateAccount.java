e89c2174a0ec794ec8eb3d13b0db61f58b2c5e92
Wilson Neely <Naruisk.art@gmail.com>
2021-11-16 13:50:57 -0600

    CreateAccount and AccountView init

--- src/main/java/com/revature/project0/render/screens/inner/CreateAccount.java
+++ src/main/java/com/revature/project0/render/screens/inner/CreateAccount.java
@@ -4,16 +4,16 @@ import com.revature.project0.render.screens.View;
 import com.revature.project0.services.AccountsService;
 import com.revature.project0.services.UsersService;
 import com.revature.project0.util.ScreenRouter;
+
 import java.util.Scanner;
 
-
-public class AccountView extends View {
+public class CreateAccount extends View {
 
     private final UsersService usersService;
     private final AccountsService accountsService;
 
-    public AccountView(Scanner consoleScanner, ScreenRouter router, UsersService usersService, AccountsService accountsService) {
-        super("accountview", "loggedin/accountview", consoleScanner, router);
+    public CreateAccount(Scanner consoleScanner, ScreenRouter router, UsersService usersService, AccountsService accountsService) {
+        super("createaccount", "loggedin/createaccount", consoleScanner, router);
         this.usersService = usersService;
         this.accountsService = accountsService;
     }

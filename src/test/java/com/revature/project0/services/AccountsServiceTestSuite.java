396b3b5086f5ec4b5b11163069cec064aff76ab7
Wilson Neely <Naruisk.art@gmail.com>
2021-11-17 02:46:48 -0600

    Unit Tests

--- src/test/java/com/revature/project0/services/AccountsServiceTestSuite.java
+++ src/test/java/com/revature/project0/services/AccountsServiceTestSuite.java
@@ -6,7 +6,86 @@ import com.revature.project0.models.Accounts;
 import com.revature.project0.models.Transactions;
 import com.revature.project0.util.Collections.LinkedList;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
 
 public class AccountsServiceTestSuite {
+    AccountsService sut;
+    AccountsDAO mockAccDAO;
+
+    @Before
+    public void testCaseSetup() {
+        mockAccDAO = mock(AccountsDAO.class);
+        sut = new AccountsService(mockAccDAO);
+    }
+
+    @After
+    public void testCaseCleanUp() {
+        sut = null;
+    }
+
+    @Test
+    public void test_updateFunds_validIn_deposit() {
+
+        // Arrange
+        LinkedList<Accounts> accountsList = new LinkedList<>();
+        Accounts accounts = new Accounts();
+        accounts.setId(1);
+        accountsList.add(accounts);
+        // Act
+        sut.updateFunds("1","150.50",accountsList,true);
+
+        // Assert
+        verify(mockAccDAO, times(1)).deposit("1", 150.50f);
+    }
+
+    @Test
+    public void test_updateFunds_validIn_withdraw() {
+
+        // Arrange
+        LinkedList<Accounts> accountsList = new LinkedList<>();
+        Accounts accounts = new Accounts();
+        accounts.setId(1);
+        accountsList.add(accounts);
+        // Act
+        sut.updateFunds("1","150.50",accountsList,false);
+
+        // Assert
+        verify(mockAccDAO, times(1)).deposit("1", -150.50f);
+    }
+
+    @Test
+    public void test_updateFunds_invalidAccountID() {
+
+        // Arrange
+        LinkedList<Accounts> accountsList = new LinkedList<>();
+        Accounts accounts = new Accounts();
+        accounts.setId(1);
+        accountsList.add(accounts);
+        // Act
+        sut.updateFunds("2","150.50",accountsList,false);
+
+        // Assert
+        verify(mockAccDAO, times(0)).deposit(anyString(), anyFloat());
+    }
+
+    @Test
+    public void test_updateFunds_amountTooLow() {
+
+        // Arrange
+        LinkedList<Accounts> accountsList = new LinkedList<>();
+        Accounts accounts = new Accounts();
+        accounts.setId(1);
+        accountsList.add(accounts);
+        // Act
+        sut.updateFunds("2","0",accountsList,false);
+
+        // Assert
+        verify(mockAccDAO, times(0)).deposit(anyString(), anyFloat());
+    }
 
 }
--- src/test/java/com/revature/project0/services/UsersServiceTestSuite.java
+++ src/test/java/com/revature/project0/services/UsersServiceTestSuite.java
@@ -1,5 +1,143 @@
 package com.revature.project0.services;
 
-public class UsersServiceTestSuite {
+import com.revature.project0.DAOs.UsersDAO;
+import com.revature.project0.exceptions.AuthenticationException;
+import com.revature.project0.exceptions.InvalidRequestException;
+import com.revature.project0.exceptions.ResourcePersistenceException;
+import com.revature.project0.models.Users;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class UsersServiceTestSuite {
+    UsersService sut;
+    UsersDAO mockUserDAO;
+
+    @Before
+    public void testCaseSetup() {
+        mockUserDAO = mock(UsersDAO.class);
+        sut = new UsersService(mockUserDAO);
+    }
+
+    @After
+    public void testCaseCleanUp() {
+        sut = null;
+    }
+
+    @Test
+    public void test_register_validUser() {
+
+        // Arrange
+        Users validUser = new Users("valid", "valid", "valid", "valid", "valid");
+        when(mockUserDAO.findUserByUsername(anyString())).thenReturn(null);
+        when(mockUserDAO.findUserByEmail(anyString())).thenReturn(null);
+        when(mockUserDAO.save(validUser)).thenReturn(validUser);
+        // Act
+        boolean actualResult = sut.register(validUser);
+
+        // Assert
+        Assert.assertTrue("Expected user to be considered valid", actualResult);
+    }
+
+    @Test(expected = ResourcePersistenceException.class)
+    public void test_register_takenUsername() {
+
+        // Arrange
+        Users validUser = new Users("valid", "valid", "valid", "valid", "valid");
+        when(mockUserDAO.findUserByUsername(anyString())).thenReturn(validUser);
+        when(mockUserDAO.findUserByEmail(anyString())).thenReturn(null);
+        when(mockUserDAO.save(validUser)).thenReturn(validUser);
+        // Act
+        boolean actualResult = sut.register(validUser);
+    }
+
+    @Test(expected = ResourcePersistenceException.class)
+    public void test_register_takenEmail() {
+
+        // Arrange
+        Users validUser = new Users("valid", "valid", "valid", "valid", "valid");
+        when(mockUserDAO.findUserByUsername(anyString())).thenReturn(null);
+        when(mockUserDAO.findUserByEmail(anyString())).thenReturn(validUser);
+        when(mockUserDAO.save(validUser)).thenReturn(validUser);
+        // Act
+        boolean actualResult = sut.register(validUser);
+    }
+
+    @Test(expected = ResourcePersistenceException.class)
+    public void test_register_takenUsernameEmail() {
+
+        // Arrange
+        Users validUser = new Users("valid", "valid", "valid", "valid", "valid");
+        when(mockUserDAO.findUserByUsername(anyString())).thenReturn(validUser);
+        when(mockUserDAO.findUserByEmail(anyString())).thenReturn(validUser);
+        when(mockUserDAO.save(validUser)).thenReturn(validUser);
+        // Act
+        boolean actualResult = sut.register(validUser);
+    }
+
+    @Test(expected = ResourcePersistenceException.class)
+    public void test_register_problemPersisting() {
+        // Arrange
+        Users validUser = new Users("valid", "valid", "valid", "valid", "valid");
+        when(mockUserDAO.findUserByUsername(anyString())).thenReturn(null);
+        when(mockUserDAO.findUserByEmail(anyString())).thenReturn(null);
+        when(mockUserDAO.save(validUser)).thenReturn(null);
+        // Act
+        boolean actualResult = sut.register(validUser);
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void test_register_invalidUser() {
+        // Arrange
+        Users invalidUser = new Users("", "valid", "valid", "valid", "valid");
+        when(mockUserDAO.findUserByUsername(anyString())).thenReturn(null);
+        when(mockUserDAO.findUserByEmail(anyString())).thenReturn(null);
+        when(mockUserDAO.save(invalidUser)).thenReturn(null);
+        // Act
+        boolean actualResult = sut.register(invalidUser);
+    }
+
+    @Test()
+    public void test_authenticate_validUser() {
+        // Arrange
+        Users validUser = new Users("valid", "valid", "valid", "valid", "valid");
+        String username = "valid";
+        String password = "valid";
+        when(mockUserDAO.findUserByUsernameAndPassword(anyString(), anyString())).thenReturn(validUser);
+        // Act
+        sut.authenticate(username, password);
+        assert sut.getSessionUser() != null;
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void test_authenticate_nullUsername() {
+        // Arrange
+        String username = null;
+        String password = "valid";
+        // Act
+        sut.authenticate(username, password);
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void test_authenticate_nullPassword() {
+        // Arrange
+        String username = "valid";
+        String password = null;
+        // Act
+        sut.authenticate(username, password);
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void test_authenticate_nullUser() {
+        // Arrange
+        String username = "valid";
+        String password = "valid";
+        when(mockUserDAO.findUserByUsernameAndPassword(anyString(), anyString())).thenReturn(null);
+        // Act
+        sut.authenticate(username, password);
+    }
 }
